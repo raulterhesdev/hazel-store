4d7a9b872127841b2ee2ebb8e046debd6b209d61
Raul Terhes <raulterhesdevelopment@gmail.com>
2020-08-27 12:45:05 +0300

    fixed some meadia queries for phones and tablets

--- client/src/components/UI/FileUpload/FileUpload.js
+++ client/src/components/UI/FileUpload/FileUpload.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import classes from './FileUpload.module.css';
 
 export class FileUpload extends Component {
   static propTypes = {};
 
   render() {
     return (
-      <div>
+      <div className={classes.Upload}>
         <input type='file' name='file' onChange={this.props.onChange} />
         <p>{this.props.error}</p>
       </div>
