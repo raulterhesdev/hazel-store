dcf33273b25e3666b7f01be7ff1faa4ab2b043f2
Raul Terhes <raulterhesdevelopment@gmail.com>
2020-08-27 13:41:14 +0300

    fixing dev impor

--- server.js
+++ server.js
@@ -59,9 +59,7 @@ const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
-  console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
-  )
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
 
 // Handle unhandled promise rejections
